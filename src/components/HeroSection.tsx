26d9329d76993dbbc729983a2e541226f53428e1
marcooamato <amatomarco111@gmail.com>
2024-10-27 16:28:21 +0100

    Home page displays the 6 pillars as logos

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,16 +1,18 @@
 // src/components/HeroSection.tsx
 
 import React from 'react';
+import { FaRunning, FaAppleAlt, FaBed, FaBrain, FaUsers, FaSmile } from 'react-icons/fa'; // Import icons
 import './HeroSection.css'; // Importing CSS for styling
 
-// Define the props interface (if you need to add props later)
 interface HeroSectionProps {}
 
 // HeroSection component
 const HeroSection: React.FC<HeroSectionProps> = () => {
   return (
     <section className="hero-section">
+      {/* Logo and title */}
       <div>
+        {/* Logo */}
         <img
           className="hero-logo"
           src="img/hero-logo.png"
@@ -20,9 +22,7 @@ const HeroSection: React.FC<HeroSectionProps> = () => {
         {/* Main title */}
         <h1 className="hero-title">Zenit</h1>
       </div>
-      {/* Hero image */}
-      
-      
+
       {/* Subtitle */}
       <p className="hero-subtitle">
         Discover science-backed insights for a healthier, more fulfilling life.
@@ -30,12 +30,30 @@ const HeroSection: React.FC<HeroSectionProps> = () => {
       
       {/* Navigation buttons */}
       <div className="hero-buttons">
-        <button className="hero-button">Exercise</button>
-        <button className="hero-button">Nutrition</button>
-        <button className="hero-button">Sleep</button>
-        <button className="hero-button">Mindset</button>
-        <button className="hero-button">Social Relationships</button>
-        <button className="hero-button">Stress Management</button>
+        <button className="hero-button">
+          <FaRunning className="button-icon" /> {/* Exercise Icon */}
+          Exercise
+        </button>
+        <button className="hero-button">
+          <FaAppleAlt className="button-icon" /> {/* Nutrition Icon */}
+          Nutrition
+        </button>
+        <button className="hero-button">
+          <FaBed className="button-icon" /> {/* Sleep Icon */}
+          Sleep
+        </button>
+        <button className="hero-button">
+          <FaBrain className="button-icon" /> {/* Mindset Icon */}
+          Mindset
+        </button>
+        <button className="hero-button">
+          <FaUsers className="button-icon" /> {/* Social Relationships Icon */}
+          Social Relationships
+        </button>
+        <button className="hero-button">
+          <FaSmile className="button-icon" /> {/* Stress Management Icon */}
+          Stress Management
+        </button>
       </div>
     </section>
   );
