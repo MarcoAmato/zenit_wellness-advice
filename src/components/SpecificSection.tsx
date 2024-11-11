ef4c51f5d6b22033a893693c94099a34c4b5bbdc
Gianmarco <gianmarco.garrisi81@gmail.com>
2024-11-11 20:06:48 +0100

    Section Question

--- src/components/SpecificSection.tsx
+++ src/components/SpecificSection.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import { FaRunning, FaAppleAlt, FaBed, FaBrain, FaUsers, FaSmile, FaUserFriends, FaTools } from 'react-icons/fa';
+import { FaRunning, FaAppleAlt, FaBed, FaBrain, FaUsers, FaSmile, FaUserFriends, FaTools, FaQuestion } from 'react-icons/fa';
 import './HeroSection.css';
+import './SpecificSection.css';
 import { Col, Container, Row } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const SpecificSection: React.FC = () => {
-  return (
-    <section className="hero-section">
+    return (
+        <section className="specific-section">
 
-      <Container>
-        <Row>
-          <Col>
-            <div className="hero-button">
-              <div className="icon-background"><FaAppleAlt className="button-icon" /></div>
-              <p>Nutrition</p>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+            <Container>
+                <Row>
+                    <Col lg="12">
+                        <div className="hero-button">
+                            <div className="icon-background"><FaAppleAlt className="button-icon" /></div>
+                            <p>Nutrition</p>
+                        </div>
+                    </Col>
+                    <Col lg="12">
+                        <div className="divQuestion">
+                            <Row>
+                                <Col lg="8">
+                                    <div className="col-left">
+                                        <p>How important is fiber intake in the diet?</p>
+                                    </div>
+                                </Col>
 
-    </section>
-  );
+                                <Col lg="4">
+                                    <div className="col-right">
+                                        <FaQuestion className="button-icon" />
+                                    </div>
+                                </Col>
+
+                            </Row>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+        </section>
+    );
 };
 
 export default SpecificSection;
