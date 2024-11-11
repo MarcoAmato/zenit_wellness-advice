7952a495b5c4013ead386bebd7da0e87e5aec31d
Gianmarco <gianmarco.garrisi81@gmail.com>
2024-11-11 18:15:48 +0100

    (Reactrap + bootstrap) responsive

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { FaRunning, FaAppleAlt, FaBed, FaBrain, FaUsers, FaSmile, FaUserFriends, FaTools } from 'react-icons/fa';
 import './HeroSection.css';
+import { Col, Container, Row } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const HeroSection: React.FC = () => {
   return (
@@ -17,33 +19,46 @@ const HeroSection: React.FC = () => {
       </div>
 
       {/* Main buttons */}
-      <div className="hero-buttons">
-        <div className="hero-button">
-          <div className="icon-background"><FaAppleAlt className="button-icon" /></div>
-          <p>Nutrition</p>
-        </div>
-        <div className="hero-button">
-          <div className="icon-background"><FaRunning className="button-icon" /></div>
-          <p>Exercise</p>
-        </div>
-        <div className="hero-button">
-          <div className="icon-background"><FaBrain className="button-icon" /></div>
-          <p>Mindset</p>
-        </div>
-        <div className="hero-button">
-          <div className="icon-background"><FaBed className="button-icon" /></div>
-          <p>Sleep</p>
-        </div>
-        <div className="hero-button">
-          <div className="icon-background"><FaUsers className="button-icon" /></div>
-          <p>Social Connections</p>
-        </div>
-        <div className="hero-button">
-          <div className="icon-background"><FaSmile className="button-icon" /></div>
-          <p>Stress Management</p>
-        </div>
-      </div>
-
+      <Container>
+        <Row>
+          <Col md="4" sm="6" xs="6">
+            <div className="hero-button">
+              <div className="icon-background"><FaAppleAlt className="button-icon" /></div>
+              <p>Nutrition</p>
+            </div>
+          </Col>
+          <Col md="4" sm="6" xs="6">
+            <div className="hero-button">
+              <div className="icon-background"><FaRunning className="button-icon" /></div>
+              <p>Exercise</p>
+            </div>
+          </Col>
+          <Col md="4" sm="6" xs="6">
+            <div className="hero-button">
+              <div className="icon-background"><FaBrain className="button-icon" /></div>
+              <p>Mindset</p>
+            </div>
+          </Col>
+          <Col md="4" sm="6" xs="6">
+            <div className="hero-button">
+              <div className="icon-background"><FaBed className="button-icon" /></div>
+              <p>Sleep</p>
+            </div>
+          </Col>
+          <Col md="4" sm="6" xs="6">
+            <div className="hero-button">
+              <div className="icon-background"><FaUsers className="button-icon" /></div>
+              <p>Social Connections</p>
+            </div>
+          </Col>
+          <Col md="4" sm="6" xs="6">
+            <div className="hero-button">
+              <div className="icon-background"><FaSmile className="button-icon" /></div>
+              <p>Stress Management</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
       {/* Bottom buttons */}
       <div className="bottom-buttons">
         <div className="bottom-button">
