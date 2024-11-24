5a5a1bd316283a087e15b1e23661851dce15df5c
marcooamato <amatomarco111@gmail.com>
2024-11-24 22:20:17 +0100

    Refactor ArticleDetails to use SourceList component for displaying sources

--- src/components/ArticleDetails/ArticleDetails.tsx
+++ src/components/ArticleDetails/ArticleDetails.tsx
@@ -3,6 +3,7 @@ import { FaQuestion, FaExternalLinkAlt } from 'react-icons/fa';
 import '../Style.css';
 import "./ArticleDetails.css";
 import { Col, Container, Row } from 'reactstrap';
+import SourceList from '../ArticleDetails/SourceList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useLocation } from 'react-router-dom';
 import { getArticleById, getPillarByArticleId, getPillarKeyByValue } from '../../utils/dataUtils';
@@ -63,24 +64,8 @@ const ArticleDetails: React.FC = () => {
                         </div>
                     </Col>
                     <Col className='marginCol' lg="12">
-                        <div className="sources-container">
-                            <h4 className="sources-title">Sources</h4>
-                            <ul className="sources-list">
-                                {article.sources.map((source, index) => (
-                                    <li key={index} className="source-card">
-                                        <FaExternalLinkAlt className="source-icon" />
-                                        <div className="source-info">
-                                            <a href={source.sourceLink} target="_blank" rel="noopener noreferrer" className="source-link">
-                                                {source.sourceTitle}
-                                            </a>
-                                            <p className="source-description">Click to read more on the external site.</p>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+                        <SourceList sources={article.sources} />
                     </Col>
-
                 </Row>
             </Container>
         </section>
